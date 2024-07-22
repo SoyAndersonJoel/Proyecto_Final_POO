c5adae09fb4490c8a22f7f2bb6351e5b3c3e6ea5
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-07-22 18:55:06 -0500

    InterfazLogin

---
+++ src/Login.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Login {
+    public JPanel mainPanel;
+    private JButton aceptarButton;
+    private JButton eliminarButton;
+    private JTextField dfgTextField;
+    private JPasswordField passwordField1;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        JFrame frame = new JFrame("Cell Tech Hub");
+        frame.setContentPane(new Login().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(1000, 630);
+        frame.setVisible(true);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
